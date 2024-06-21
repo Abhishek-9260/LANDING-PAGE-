fc311e1758971017dba662534440298b9319ffcc
Abhishek-9260 <168534559+Abhishek-9260@users.noreply.github.com>
2024-06-21 12:22:57 +0530

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,19 @@
+// Set the countdown date to 5 days from now
+var countDownDate = new Date(new Date().getTime() + 5 * 24 * 60 * 60 * 1000).getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "TIME'S UP!";
+  }
+}, 1000);
